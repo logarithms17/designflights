1fa290a5466861576713e42bb0d113b99dde882f
Neil Bryan Apostol <neilbryanapostol@gmail.com>
2025-01-11 16:14:06 +0800

    modify links

--- src/components/landingpage-components/MainNavigation.tsx
+++ src/components/landingpage-components/MainNavigation.tsx
@@ -1,13 +1,13 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import logo from '../../assets/logo.png'
 const MainNavigation = () => {
   return (
       <div className="grid grid-cols-2 gap-4 mb-[240px] py-10 px-4 sm:px-8 md:px-16 lg:px-20">
           <div><img src={logo} alt="logo" className="" /></div>
           <div className='justify-self-end flex gap-4 font-semibold text-xl'>
-              <Link to="/">work</Link>
-              <Link to="/about">about</Link>
-              <Link to="/contact">contact</Link>
+              <NavLink to="/" className={({isActive} : {isActive: boolean}) => isActive ? 'text-blue-700 underline underline-offset-4' : ''}>work</NavLink>
+              <NavLink to="/about" className={({isActive} : {isActive: boolean}) => isActive ? 'text-blue-700 underline underline-offset-4' : ''}>about</NavLink>
+              <NavLink to="/contact" className={({isActive} : {isActive: boolean}) => isActive ? 'text-blue-700 underline underline-offset-4' : ''}>contact</NavLink>
           </div>
     </div>
   )
--- src/components/landingpage-components/WorkSamples.tsx
+++ src/components/landingpage-components/WorkSamples.tsx
@@ -17,6 +17,7 @@ import { useState } from 'react'
 
 
 const WorkSamples = () => {
+  
   const [isSwiped, setIsSwiped] = useState(false)
 
   const toggleIsSwiped = () => {
